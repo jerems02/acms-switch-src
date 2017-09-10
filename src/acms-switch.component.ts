4bd5777652ff7b93b0e2f9c6cd3f23fbb36afb29
Jérémie Stezycki <jeremie.stezycki@sylpheo.com>
2017-09-10 18:25:38 +0200

    Adds a state input to change dynamically the state of the switch

--- src/acms-switch.component.ts
+++ src/acms-switch.component.ts
@@ -1,14 +1,21 @@
-import { Component, Input, Output, EventEmitter } from '@angular/core';
+import {Component, Input, Output, EventEmitter, ViewChild, OnInit, OnChanges, SimpleChanges} from '@angular/core';
 
 @Component({
   selector: 'acms-switch',
   templateUrl: './acms-switch.component.html',
   styleUrls: ['./acms-switch.component.css']
 })
-export class AcmsSwitchComponent {
+export class AcmsSwitchComponent implements OnChanges{
 
+
+  @Input() state: boolean = false;
   @Input() id;
   @Output() switchEvent: EventEmitter<any> = new EventEmitter<any>();
+  @ViewChild('acmsSwitch') acmsSwitch;
+
+  ngOnChanges(changes: SimpleChanges): void {
+    this.acmsSwitch.nativeElement.checked = this.state;
+  }
 
   constructor() { }
 
